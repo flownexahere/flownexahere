0a7a9b205c1de64f30438dac96674f209930d108
Bilal Imran <acc.bilalimran@gmail.com>
2026-04-25 17:30:51 +0500

    enable netlify SSR support

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     build: {
-      outDir: "dist/client"
+      outDir: "dist",
     }
   }
 });
